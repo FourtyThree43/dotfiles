da5faed6bc2472fd33ef10183e1503f89167912f
FourtyThree43 <shaqmwa@outlook.com>
2023-04-12 20:13:14 +0300

    Updated with emoticons

--- run_pycodestyle.sh
+++ run_pycodestyle.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+## ------------ COMPLEX EMOTICONS ------------ ##
+EMOTICONS=(
+    "¯\_(ツ)_/¯" "(╯°□°）╯︵ ┻━┻" "(ง'̀-'́)ง" "( ͡° ͜ʖ ͡°)" "(╯︵╰,)"
+    "(っ◕‿◕)っ" "(ಥ﹏ಥ)" "(ง •̀_•́)ง" "(づ｡◕‿‿◕｡)づ" "(╬ಠ益ಠ)"
+    "(ʘ‿ʘ)" "(╯°Д°）╯︵/(.□ . \)" "( ͡°👅 ͡°)" "(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧" "(ಠ‿ಠ)"
+    "( ͡°⊖ ͡°)" "(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧" "(ง'̀-'́)ง" "(ʘ‿ʘ)" "(◕ᴗ◕✿)"
+    "(✿◠‿◠)" "(╯°□°)╯︵ ʞooqǝɔɐɟ" "(ง'̀-'́)ง╭★╮(ಠ۝ಠ)╭★╮(ง'̀-'́)ง"
+    "┬┴┬┴┤(･_├┬┴┬┴" "( ͡❛ ͜ʖ ͡❛)" "(╯°□°)╯︵ ┻━┻ ︵ ╯(°□° ╯)" "( ͡~ ͜ʖ ͡°)"
+    "(ﾉ≧∀≦)ﾉ ‥…━━━★" "( ･_･)♡" "( ͡° ͜ʖ ͡°)╭★╮" "( ͡• ͜ʖ ͡•)"
+    "٩(◕‿◕｡)۶" "( ͠° ͟ʖ ͡°)" "(ง'̀-'́)ง✧" "┌( ಠ‿ಠ)┘" "(≧◡≦)"
+    "(ง ื▿ ื)ว" "ಠ_ಠ" "(ง •̀_•́)งง" "ᕙ(⇀‸↼‶)ᕗ" "(ง°ل͜°)ง"
+    "(⊙_◎)" "(⊙_◎)ノ" "(ﾉﾟ0ﾟ)ﾉ~" "ᕕ( ᐛ )ᕗ" "(≧∇≦)/"
+    "(づ￣ ³￣)づ" "✌(-‿-)✌" "(ง^ᗜ^)ง" "ᕕ(⌐■_■)ᕗ ♪♬"
+)
+
+# Generate a random number between 0 and the length of the array
+RANDOM_INDEX=$((RANDOM % ${#EMOTICONS[@]}))
+
 ## ------------ COLORS ------------ ##
 
 Color_Off='\033[0m' # Text Reset
@@ -23,9 +41,9 @@ On_Blue='\033[44m'  On_Purple='\033[45m' On_Cyan='\033[46m'  On_White='\033[47m'
 check_output=$(find . -name '*.py' ! -name '*main*' ! -name '*test*' -exec pycodestyle --show-source --statistics {} +)
 
 if [[ $check_output == "" ]]; then
-  printf "${BGreen}Check Done!${Color_off} ${BYellow}No errors found.${Color_off}\n"
+  printf "${BGreen}Check Done!${Color_off} ${BYellow}No errors found. ${White}${EMOTICONS[$RANDOM_INDEX]}${Color_off}\n"
 else
-  printf "${BGreen}Check Done!${Color_off} ${BPurple}Errors found:${Color_off}\n"
+  printf "${BGreen}Check Done!${Color_off} ${BPurple}Errors found:${Color_off} ${White}${EMOTICONS[$RANDOM_INDEX]}${Color_off}\n"
   #printf "${White}$check_output${Color_off}\n"
   #echo "$check_output" | perl -F: -lane 'if ($F[0] =~ /^\.\//) {printf "\033[0;33m%s\033[0m:%s:%s\033[1;31m:%s:\033[0m \033[1;36m%s\033[0m\n", $F[0], $F[1], $F[2], $F[3], $F[4]} else {print}'
   echo "$check_output" | awk -F':' '{if (index($1, "./") == 1) printf "\033[0;33m%s\033[0m:%s:%s\033[1;31m:%s:\033[0m \033[1;36m%s\033[0m\n", $1, $2, $3, $4, $5; else print}'
